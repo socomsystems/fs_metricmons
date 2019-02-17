ad6151eeeaa900a02649b22d687f0c1283ef2b3f
Troy Perkins <socomsystems@users.noreply.github.com>
2019-02-17 07:53:53 -0600

    Update cc-status.sh
    Divide channels by 2 to reflect accurate call count.
    Added sending active calls count to NetData!

--- cc-status.sh
+++ cc-status.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Capture number of channels in use and send to syslog
 MAX_CALLS=1000
 CHANNEL_COUNT=$(/usr/bin/fs_cli -x "show channels count" | awk '/total/ {print $1}')
 /usr/bin/logger -p local0.info -t cc-status -i has reached $CHANNEL_COUNT calls.
+
+# Divide channels by 2 to reflect accurate call count
+if [ "$CHANNEL_COUNT" = "0" ]; then 
+   fscallcount=0; 
+else fscallcount=`echo $CHANNEL_COUNT/2 | bc`;
+fi
+
+# Send call count to NetData via statsd!
+echo "callcount:$fscallcount:|g" | nc -w 1 -u localhost 8125
 
 if [ $CHANNEL_COUNT -gt $MAX_CALLS ]; then
         FROM="ALERT@domain.name"
@@ -13,7 +23,5 @@ Alert, node call count is $CHANNEL_COUNT.  Max calls threshold is $MAX_CALLS.
 END
 
 /usr/bin/logger -p local0.crit -t cc-status -i has reached $CHANNEL_COUNT calls, marking critical
-
 fi
-
 /usr/bin/logger -p local0.info -t cc-status -i has reached $CHANNEL_COUNT calls, marking acceptable
